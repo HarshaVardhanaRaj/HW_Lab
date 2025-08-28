e65be97288f6679f1774a2b1e8e8a56807ab667b
P Harsha Vardhana Raj <106435268+HarshaVardhanaRaj@users.noreply.github.com>
2025-08-28 21:13:27 +0530

    Add files via upload

--- Menu Driven Calculator.py
+++ Menu Driven Calculator.py
@@ -1,15 +1,18 @@
+import sys
 a = float(input("Enter value of a: "))
 b = float(input("Enter value of b: "))
-choice = int(input("Enter 1 to Add, 2 to Subtract, 3 to multiply, or 4 to divide: "))
-match(choice):
-    case 1:
-        print(a,'+',b,'=',(a+b))
-    case 2:
-        print(a,'-',b,'=',(a-b))
-    case 3:
-        print(a,'x',b,'=',(a*b))
-    case 4:
-        print(a,'/',b,'=',(a/b))
-    case _:
-        print("Invalid Option")
-        
+while(True):
+    ch = int(input("Enter 1 to Add, 2 to Subtract, 3 to multiply, 4 to divide, or 5 to exit: "))
+    if(ch==1):
+            print(a,'+',b,'=',(a+b))
+    elif(ch==2):
+            print(a,'-',b,'=',(a-b))
+    elif(ch==3):
+            print(a,'x',b,'=',(a*b))
+    elif(ch==4):
+            print(a,'/',b,'=',(a/b))
+    elif(ch==5):
+            print("Operation Over.")
+            sys.exit(0)
+    else: 
+            print("Invalid Option")
