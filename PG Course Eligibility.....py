18346db4f353ef462ad6783788a1e03d7e879706
P Harsha Vardhana Raj <106435268+HarshaVardhanaRaj@users.noreply.github.com>
2025-09-15 15:12:03 +0530

    Update PG Course Eligibility.....py

--- PG Course Eligibility.....py
+++ PG Course Eligibility.....py
@@ -1,4 +1,13 @@
 x = float(input("Enter Class X Percentage: "))
 xii = float(input("Enter Class XII Percentage: "))
-ug = float(input("Enter UG Percentage: "))
-ug_stream = input("Enter your UG Percentage: ")
+ug_cgpa = float(input("Enter UG CGPA: "))
+ug_stream = input("Enter your attended UG Stream: ")
+pg_stream = input("Enter your preferred PG Stream: ")
+if(pg_stream!=ug_stream):
+    print("Since you are preferring a different PG Program your CGPA is reduced by 1")
+    ug_cgpa-=1
+    print("Your considered CGPA =",ug_cgpa)
+if(x>=75 and xii>=75 and ug_cgpa>=7.5):
+    print("You are eligible for the preferred PG course.")
+else:
+    print("You are ineligible for the preferred PG course.")
