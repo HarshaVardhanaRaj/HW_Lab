e645870e30a5ed35449c63ccb03d6b130070e59c
P Harsha Vardhana Raj <106435268+HarshaVardhanaRaj@users.noreply.github.com>
2025-09-15 15:16:09 +0530

    Update PG Course Eligibility.....py

--- PG Course Eligibility.....py
+++ PG Course Eligibility.....py
@@ -7,6 +7,9 @@ if(pg_stream!=ug_stream):
     print("Since you are preferring a different PG Program your CGPA is reduced by 1")
     ug_cgpa-=1
     print("Your considered CGPA =",ug_cgpa)
+print("Required 10th Percentage = 75 \t Your percentage =",x)
+print("Required 12th Percentage = 75 \t Your percentage =",xii)
+print("Required CGPA in UG = 7.5 \t Your CGPA =",ug_cgpa)
 if(x>=75 and xii>=75 and ug_cgpa>=7.5):
     print("You are eligible for the preferred PG course.")
 else:
