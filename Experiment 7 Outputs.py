c113bf482c160c543b7c184d973ae9d930b96cf8
P Harsha Vardhana Raj <106435268+HarshaVardhanaRaj@users.noreply.github.com>
2025-09-18 11:33:27 +0530

    Add files via upload

---
+++ Experiment 7 Outputs.py
@@ -0,0 +1,52 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/Lenovo/Documents/Harsha Vardhana Raj - AI&DS - Python Lab Works/Aliasing Example.py
+1402307698880
+1402307698880
+['Milk', 'Bread', 'Eggs', 'Butter']
+['Milk', 'Bread', 'Eggs', 'Butter']
+>>> 
+= RESTART: C:/Users/Lenovo/Documents/Harsha Vardhana Raj - AI&DS - Python Lab Works/Cloning Example.py
+3112368234688
+3112368192640
+['Milk', 'Bread', 'Eggs']
+['Milk', 'Bread', 'Eggs', 'Butter']
+>>> 
+= RESTART: C:/Users/Lenovo/Documents/Harsha Vardhana Raj - AI&DS - Python Lab Works/List Comprehension Example 1.py
+Enter num: 1
+Enter num: 2
+Enter num: 3
+Enter num: 4
+Enter num: 5
+Before adding 5: 
+List =  [1, 2, 3, 4, 5]
+After adding 5 to each element: 
+[6, 7, 8, 9, 10]
+>>> 
+= RESTART: C:/Users/Lenovo/Documents/Harsha Vardhana Raj - AI&DS - Python Lab Works/List Comprehension Example 2.py
+Enter no. of Elements: 5
+Enter an integer: 1
+Enter an integer: 2
+Enter an integer: 3
+Enter an integer: 4
+Enter an integer: 5
+Original List:  [1, 2, 3, 4, 5]
+Even numbers are:  [2, 4]
+
+= RESTART: C:/Users/Lenovo/Documents/Harsha Vardhana Raj - AI&DS - Python Lab Works/List Comprehension Example 3.py
+Enter no. of Elements: 5
+Enter an integer: 1
+Enter an integer: 2
+Enter an integer: 3
+Enter an integer: 4
+Enter an integer: 5
+Original List:  [1, 2, 3, 4, 5]
+Squares: [1, 4, 9, 16, 25]
+Cubes: [1, 8, 27, 64, 125]
+Even nos: [2, 4]
+
+= RESTART: C:/Users/Lenovo/Documents/Harsha Vardhana Raj - AI&DS - Python Lab Works/List Comprehension Example 4.py
+Fig is printed from else part
+Orange is printed from else part
+Jackfruit is printed from if part
